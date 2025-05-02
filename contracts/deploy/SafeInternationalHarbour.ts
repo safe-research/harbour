dffe743980d3c7357057922b961b67cc644aba9e
Mikhail <16622558+mmv08@users.noreply.github.com>
2025-05-02 16:11:36 +0200

    chore: enhance Hardhat configuration and deployment task for custom chains
    - Added support for Gnosis custom chain in Hardhat configuration to enable contract verification on Gnosisscan.
    - Updated deployAndVerify task to utilize custom Etherscan URLs for verification based on the current chain ID.
    - Cleaned up comments in SafeInternationalHarbour deployment script for clarity.

--- contracts/deploy/SafeInternationalHarbour.ts
+++ contracts/deploy/SafeInternationalHarbour.ts
@@ -4,14 +4,12 @@ const func: DeployFunction = async ({ getNamedAccounts, deployments }) => {
 	const { deterministic, log } = deployments;
 	const { deployer } = await getNamedAccounts();
 
-	// Deploy SafeInternationalHarbour deterministically using CREATE2
 	const result = await deterministic("SafeInternationalHarbour", {
 		from: deployer,
 		args: [],
 		log: true,
 	});
 
-	// execute the deployment
 	await result.deploy();
 
 	log(`SafeInternationalHarbour deployed at ${result.address}`);
--- contracts/tasks/deployAndVerify.ts
+++ contracts/tasks/deployAndVerify.ts
@@ -4,7 +4,15 @@ task("deploy-and-verify", "Deploys and verifies Harbour contracts")
 	.addOptionalParam("tags", "Tags to deploy")
 
 	.setAction(async (taskArgs, hre) => {
+		const chainId = Number.parseInt(await hre.getChainId());
+		const etherscanCustomChainsEntry = hre.config.etherscan.customChains?.find((chain) => chain.chainId === chainId);
+		const needsCustomEtherscanUrl = !!etherscanCustomChainsEntry;
+
 		await hre.run("deploy", { tags: taskArgs.tags });
-		await hre.run("etherscan-verify", { forceLicense: true, license: "LGPL-3.0" });
+		await hre.run("etherscan-verify", {
+			forceLicense: true,
+			license: "LGPL-3.0",
+			apiUrl: needsCustomEtherscanUrl ? etherscanCustomChainsEntry?.urls.apiURL : undefined,
+		});
 		await hre.run("sourcify", { tags: taskArgs.tags });
 	});
