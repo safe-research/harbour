b284a5a4148a56760dccf08ca55e388b27f2e5df
Richard Meissner <msc.meissner@gmail.com>
2025-08-04 19:27:59 +0000

    Remove unused method

--- validator/src/erc4337/paymaster.ts
+++ validator/src/erc4337/paymaster.ts
@@ -1,37 +1,7 @@
 import {
-	type Address,
 	encodePacked,
-	getAddress,
 	type Hex,
-	hexToBigInt,
-	zeroAddress,
 } from "viem";
-
-export function decodePaymasterAndData(paymasterAndData: Hex): {
-	paymaster: Address;
-	paymasterVerificationGasLimit: bigint;
-	paymasterPostOpGasLimit: bigint;
-	paymasterData: Hex;
-} {
-	if (paymasterAndData.length !== 130) {
-		return {
-			paymaster: zeroAddress,
-			paymasterVerificationGasLimit: 0n,
-			paymasterPostOpGasLimit: 0n,
-			paymasterData: "0x",
-		};
-	}
-	return {
-		paymaster: getAddress(paymasterAndData.slice(0, 42)),
-		paymasterVerificationGasLimit: hexToBigInt(
-			`0x${paymasterAndData.slice(42, 74)}`,
-		),
-		paymasterPostOpGasLimit: hexToBigInt(
-			`0x${paymasterAndData.slice(74, 106)}`,
-		),
-		paymasterData: `0x${paymasterAndData.slice(106, 130)}`,
-	};
-}
 
 export function encodePaymasterData(params?: {
 	validAfter?: number;
