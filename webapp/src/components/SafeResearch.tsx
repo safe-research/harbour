5d2c1c89054d9869952481354db54542280059f9
Tobias Schubotz <1380529+tschubotz@users.noreply.github.com>
2025-08-07 12:59:02 +0200

    Fix format issues

--- webapp/src/components/SafeResearch.tsx
+++ webapp/src/components/SafeResearch.tsx
@@ -1,46 +1,46 @@
-import { Alert, Link, Typography } from "@mui/material"
-import type { ReactNode } from "react"
+import { Alert, Link, Typography } from "@mui/material";
+import type { ReactNode } from "react";
 
 export const CustomLink = ({
-  to,
-  children,
+	to,
+	children,
 }: {
-  to: string
-  children: ReactNode
+	to: string;
+	children: ReactNode;
 }) => {
-  return (
-    <Link
-      href={to}
-      target="_blank"
-      rel="noopener"
-      underline="none"
-      color="inherit"
-      sx={{ ':hover': { color: '#12ff80' } }}
-    >
-      {children}
-    </Link>
-  )
-}
+	return (
+		<Link
+			href={to}
+			target="_blank"
+			rel="noopener"
+			underline="none"
+			color="inherit"
+			sx={{ ":hover": { color: "#12ff80" } }}
+		>
+			{children}
+		</Link>
+	);
+};
 
 export const SafeResearchBanner = () => {
-  return (
-    <Alert severity="warning">
-      This demo is an experimental beta release. Code is not audited. Use at
-      your own risk.
-    </Alert>
-  )
-}
+	return (
+		<Alert severity="warning">
+			This demo is an experimental beta release. Code is not audited. Use at
+			your own risk.
+		</Alert>
+	);
+};
 
 export const SafeResearchFooter = ({ repo }: { repo: string }) => {
-  return (
-    <Typography>
-      <CustomLink to="https://github.com/safe-research">
-        Built by Safe Research
-      </CustomLink>
-      &nbsp;&hearts;&nbsp;
-      <CustomLink to={`https://github.com/safe-research/${repo}`}>
-        Source on GitHub
-      </CustomLink>
-    </Typography>
-  )
-}
+	return (
+		<Typography>
+			<CustomLink to="https://github.com/safe-research">
+				Built by Safe Research
+			</CustomLink>
+			&nbsp;&hearts;&nbsp;
+			<CustomLink to={`https://github.com/safe-research/${repo}`}>
+				Source on GitHub
+			</CustomLink>
+		</Typography>
+	);
+};
