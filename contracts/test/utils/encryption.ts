fe29dce6557c818a7033ddc5fb907989ccf311c1
Nicholas Rodrigues Lordello <n@lordello.net>
2025-08-14 14:10:28 +0200

    Notarized Transaction Registration (#54)
    This PR refactors the `registerTransaction` into an `enqueuTransaction`
    function that has the same parameters as before. However, the new
    implementation distinguishes between a few things:
    
    1. `encryptionBlob` is not documented as being an application specific
    blob that is related to a Safe transaction hash, but not specifically
    the encrypted transaction data itself. It can also potentially contain
    just wrapped encryption keys for a previously emitted encrypted Safe
    transaction.
    2. The transaction registrations are keyed per _notary_ and not per
    signer. This separates the role of generating the encrypted transaction
    data from the role of signing. In the case of Harbour without relaying
    the `msg.sender` is the notary. This can be a proposer account that just
    creates a transaction and encrypts it for each of the signers. In a
    (future) relayed version, it can be the paymaster address to represent
    the harbour validator set.
    3. Signatures are emitted as separate events and recorded in storage
    prevent signature replaying
    
    One notable disadvantage of this structure is that you need 4 RPC
    roundtrips instead of 2 to query all of the transactions and signatures
    (although, you can now query all of the transactions and their data in 2
    roundtrips, and separately query all of the signatures for a safe TX
    hash in another 2 roundtrips):
    
    1. `eth_call: retrieveRegistrations(chain, safe, nonce, notary)` to get
    all of the registered transaction data from a trusted `notary` (for
    arguments sake, lets say some proposer account; in the future, the
    Harbour paymaster meaning that it was approved by the harbour validator
    set).
    2. A batched RPC `eth_getLogs` filtering on the event topic, the UID and
    block number from the call in step 1 (for _each_ registration returned
    that way)
    3. A batched RPC `eth_call: retrieveSignatures([signers], safeTxHash)`
    for each safe transaction hash from step 2, and each signer you care
    about
    4. A batched RPC `eth_getLogs` filtered on the signature event topic and
    the signer for the block range returned in step 3.
    
    Note that it is possible to split the function into multiple separate
    functions: `enqueueTransaction`, `registerTransaction`, and
    `registerSignature`. I do think that it will be common to submit a
    transaction registration and signature _together_ so it does make sense
    to have the current function anyway.

--- contracts/test/utils/encryption.ts
+++ contracts/test/utils/encryption.ts
@@ -1,6 +1,7 @@
 import zlib from "node:zlib";
-import { type BytesLike, ethers } from "ethers";
-import jose from "jose";
+import { x25519 } from "@noble/curves/ed25519";
+import { type BytesLike, ethers, type Signer } from "ethers";
+import jose, { type GeneralJWE } from "jose";
 import { rlpDecodeSafeTransaction, rlpEncodeSafeTransaction, type SafeTransaction } from "./safeTx";
 
 type PublicKey = string;
@@ -19,6 +20,27 @@ async function randomX25519KeyPair(): Promise<X25519Key> {
 	};
 }
 
+async function deterministicX25519KeyPair(signer: Signer, context: BytesLike): Promise<X25519Key> {
+	const siwe = `Fake SIWE ${await signer.getAddress()} ${ethers.hexlify(context)}`;
+	const entropy = await signer.signMessage(siwe);
+	const { secretKey, publicKey } = x25519.keygen(ethers.getBytes(ethers.keccak256(entropy)));
+
+	// TODO: we should actually compute the PKCS#8 format, for now just guess based on computed
+	// `crypto.subtle.exportKey("pkcs8", privateKey)` for randomly generated `privateKey`s.
+	const pkcs8 = ethers.getBytes(
+		ethers.concat([
+			"0x302e020100300506032b656e04220420", // experimentally determined PKCS#8 header
+			secretKey,
+		]),
+	);
+	const decryptionKey = await crypto.subtle.importKey("pkcs8", pkcs8, { name: "X25519" }, true, ["deriveBits"]);
+
+	return {
+		decryptionKey,
+		encryptionKey: ethers.hexlify(publicKey),
+	};
+}
+
 async function exportX25519PublicKey(publicKey: CryptoKey): Promise<PublicKey> {
 	const raw = await crypto.subtle.exportKey("raw", publicKey);
 	return ethers.hexlify(new Uint8Array(raw));
@@ -28,6 +50,19 @@ async function importX25519PublicKey(publicKey: PublicKey): Promise<CryptoKey> {
 	const raw = ethers.getBytes(publicKey);
 	const key = await crypto.subtle.importKey("raw", raw, { name: "X25519" }, true, []);
 	return key;
+}
+
+function encodeJwe(jwe: unknown): string {
+	const data = new TextEncoder().encode(JSON.stringify(jwe));
+	const compressed = zlib.brotliCompressSync(data);
+	return `0x${compressed.toString("hex")}`;
+}
+
+function decodeJwe(encryptionBlob: BytesLike): GeneralJWE {
+	const data = ethers.getBytes(encryptionBlob);
+	const decompressed = zlib.brotliDecompressSync(data);
+	const jwe = JSON.parse(new TextDecoder().decode(decompressed));
+	return jwe as unknown as GeneralJWE;
 }
 
 async function encryptSafeTransaction(safeTx: SafeTransaction, recipients: PublicKey[]): Promise<string> {
@@ -41,18 +76,13 @@ async function encryptSafeTransaction(safeTx: SafeTransaction, recipients: Publi
 	}
 
 	const jwe = await (builder ?? encrypt).encrypt();
-	const data = new TextEncoder().encode(JSON.stringify(jwe));
-	const compressed = zlib.brotliCompressSync(data);
-
-	return `0x${compressed.toString("hex")}`;
+	return encodeJwe(jwe);
 }
 
-async function decryptSafeTransaction(encryptedSafeTx: BytesLike, decryptionKey: CryptoKey): Promise<SafeTransaction> {
-	const data = ethers.getBytes(encryptedSafeTx);
-	const decompressed = zlib.brotliDecompressSync(data);
-	const jwe = JSON.parse(new TextDecoder().decode(decompressed));
+async function decryptSafeTransaction(encryptionBlob: BytesLike, decryptionKey: CryptoKey): Promise<SafeTransaction> {
+	const jwe = decodeJwe(encryptionBlob);
 	const { plaintext } = await jose.generalDecrypt(jwe, decryptionKey);
 	return rlpDecodeSafeTransaction(plaintext);
 }
 
-export { randomX25519KeyPair, encryptSafeTransaction, decryptSafeTransaction };
+export { randomX25519KeyPair, deterministicX25519KeyPair, encryptSafeTransaction, decryptSafeTransaction };
