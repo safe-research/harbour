bcf3c72f269acf3463bf5e14742e5479812e9166
Nicholas Rodrigues Lordello <nick@safe.dev>
2025-09-09 16:49:13 +0200

    formatting

--- webapp/src/components/settings/SettingsForm.tsx
+++ webapp/src/components/settings/SettingsForm.tsx
@@ -119,7 +119,7 @@ function SettingsForm({
 						type="button"
 						className="px-3 py-2 ml-4 border border-transparent text-base font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 disabled:opacity-50 disabled:cursor-not-allowed transition-colors duration-200"
 						onClick={onUseEncryption}
-                        aria-label="use encryption"
+						aria-label="use encryption"
 					>
 						{"🔐"}
 					</button>
